6ee42c5ec209f312ae7fab94e0b427a13efc3e66
sanbei120 <jonlinpei@gmail.com>
2015-03-24 10:50:01 -0700

    Initial Commit

---
+++ CodePathAirbnb/BoxesViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BoxesViewController.swift
+//  CodePathAirbnb
+//
+//  Created by Jonlin Pei on 3/4/15.
+//  Copyright (c) 2015 Jonlin Pei. All rights reserved.
+//
+
+import UIKit
+
+class BoxesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
